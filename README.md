3172a9b2efda9c9b7f4b6a1bc14c453186733243
Bryan Fillmer <bryan.fillmer@effectiveui.com>
2016-04-24 06:32:08 -0600

    Initial port of webpack code and such.

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# React & Flyd Boilerplate
+
+Playing with leveraging React & Flyd for a fully reactive boilerplate.
+
+```shell
+git clone https://github.com/bfillmer/react-flyd.git
+cd react-flyd
+npm install
+npm run dev # Point browser to: http://localhost:8080
+```
---
+++ webpack.config.babel.js
@@ -0,0 +1,43 @@
+
+import path from 'path';
+import webpack from 'webpack';
+
+const app = path.resolve(__dirname, 'app');
+const nodeModules = path.resolve(__dirname, 'node_modules');
+
+module.exports = {
+  resolve: {
+    root: app,
+    extensions: [
+      '',
+      '.js',
+      '.jsx',
+    ],
+  },
+  entry: {
+    app: path.resolve(app, 'index.js'),
+    vendors: [
+      'react',
+      'react-dom',
+      'socrates',
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/bin/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js||jsx)$/,
+        loaders: ['babel-loader'],
+        exclude: nodeModules,
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+  ],
+
+};
